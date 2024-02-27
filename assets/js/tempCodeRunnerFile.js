2e7aaf8b9e48fd114596b6985b80d4fc6ae4909f
Mkhabele Mikejunior Mark Mashaole <137772011+mkhmik004@users.noreply.github.com>
2024-02-27 19:35:04 +0200

    redesign of education page
    flow and consistancy design in education page

---
+++ assets/js/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      setTimeout(() => {
+        preloader.classList.add('loaded');
+      }, 1000);
+      setTimeout(() => {
+        preloader.remove();
+      }, 2000);
+    });
+  }
